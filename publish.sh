6b8f342bdbcefdefcf3d4740e685a61cfa22bb07
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2018-03-09 08:44:30 +0100

    No more sudo

--- publish.sh
+++ publish.sh
@@ -4,5 +4,5 @@ set -e
 VERSION=$(cat VERSION.txt)
 
 ./build.sh
-sudo docker tag dips/poormans-cache:latest vt-optimus-solr02:5000/dips/poormans-cache:$VERSION
-sudo docker push vt-optimus-solr02:5000/dips/poormans-cache:$VERSION
+docker tag dips/poormans-cache:latest vt-optimus-solr02:5000/dips/poormans-cache:$VERSION
+docker push vt-optimus-solr02:5000/dips/poormans-cache:$VERSION
